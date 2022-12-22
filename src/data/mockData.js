e34d6156d97c2bceb8885ac9ee86baa27af86b66
julian.johnson.10 <julian.johnson.10@outlook.com>
2022-12-21 21:52:08 -0800

    initial commit

---
+++ src/data/mockData.js
@@ -0,0 +1,44 @@
+const mockDataTeam = [
+{
+    id: 1,
+    name: "Julian Johnson",
+    email: "julian@gmail.com",
+    age: 28,
+    phone: "(123)-123-1234",
+    access: "admin",
+},
+{
+    id: 2,
+    name: "James Johnson",
+    email: "james@gmail.com",
+    age: 37,
+    phone: "(123)-123-2345",
+    access: "manager",
+},
+{
+    id: 3,
+    name: "Rick Astley",
+    email: "rick@gmail.com",
+    age: 65,
+    phone: "(123)-123-1234",
+    access: "reader",
+},
+{
+    id: 4,
+    name: "Julian Johnson",
+    email: "julian@gmail.com",
+    age: 28,
+    phone: "(123)-123-1234",
+    access: "admin",
+},
+{
+    id: 5,
+    name: "Julian Johnson",
+    email: "julian@gmail.com",
+    age: 28,
+    phone: "(123)-123-1234",
+    access: "manager",
+}
+]
+
+export default mockDataTeam;
---
+++ src/scenes/dashboard/index.jsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+
+const Dashboard = () => {
+  return (
+    <Box m="10px" >
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+      </Box>
+    </Box>
+  );
+};
+
+export default Dashboard;
